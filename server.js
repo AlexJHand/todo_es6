87c63c52c2997c2376a5ef7a9373cd813a755074
Alex Hand <alex.j.hand@gmail.com>
2018-01-01 16:17:44 -0600

    Fixed base get from repeating

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
-app.use('/*', index);
+app.use('/', index);
 
 // Listener
 app.listen(port, function () {
