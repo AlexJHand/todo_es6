eeeeec822e9d02daeb97ce6f54eb6b44dc0707f8
Alex Hand <alex.j.hand@gmail.com>
2018-01-02 13:29:18 -0600

    Client.js controller sorted out

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,6 @@
 // Create angular app in js
 let myApp = angular.module('myApp', ['ngRoute']);
 
-// // Create basic routing config
-// myApp.config(($routeProvider, $locationProvider) => {
-//     $locationProvider.hashPrefix('');
-//     $routeProvider.when('/', {
-//         templateUrl: '../views/index.html',
-//         controller: 'IndexController as ic',
-//     }).otherwise('/');
-//     // Added for the removal of '#' characters in url
-//     $locationProvider.html5Mode(true);
-// });
-
 // Create basic routing config
 myApp.config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
