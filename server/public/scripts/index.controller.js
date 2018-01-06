d6f0dcced308075a89351122d89295f39050d468
Alex Hand <alex.j.hand@gmail.com>
2018-01-06 10:26:40 -0600

    Added date box

--- server/public/scripts/index.controller.js
+++ server/public/scripts/index.controller.js
@@ -2,8 +2,9 @@ myApp.controller('IndexController', function(TodoService) {
     console.log('In IndexController');
     let vm = this;
 
-    vm.displayText = (text) => {
+    vm.displayText = (text, date) => {
         console.log('Display text', text);
+        console.log('Date', date);
         vm.addedText = text;
     }
 })
